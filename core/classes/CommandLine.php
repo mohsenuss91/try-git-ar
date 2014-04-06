8f43e7d3f772b3520b80284a69aa4533ffa53c46
zendyani <zendyani@gmail.com>
2014-04-06 21:09:35 +0100

    replace tab with space on core php file

--- core/classes/CommandLine.php
+++ core/classes/CommandLine.php
@@ -8,19 +8,19 @@
  **/
 class CommandLine extends Git {
 
-	public function __construct() {
-	        parent::__construct();
-	}
+    public function __construct() {
+            parent::__construct();
+    }
 
-	/**
-	 * This function exec command and return the result of it.
-	 * 
-	 * @param $command {string} git command like "git init".
-	 * @param $numOfChallenge {int} challenge number "status".
-	 * @return {string,bool}
-	 * @author Hachem Zerdia
-	 **/
-	public function execCommand($command,$numOfChallenge) {
+    /**
+     * This function exec command and return the result of it.
+     * 
+     * @param $command {string} git command like "git init".
+     * @param $numOfChallenge {int} challenge number "status".
+     * @return {string,bool}
+     * @author Hachem Zerdia
+     **/
+    public function execCommand($command,$numOfChallenge) {
         // Explode command for testing.
         $commandExec = $this->explodeCommand($command);
         // Test command if it's valid.
@@ -28,135 +28,135 @@ class CommandLine extends Git {
         
         // If command is valid run 1 of cases below,
         // and nubmer or challenge status is into account.
-		if($isCommandValid === true) {
-			switch ($numOfChallenge) {
-			case 1: 
-				$this->updateStatus();
-				return $this->init($numOfChallenge);
-				break;
-			case 2:
-				$this->updateStatus();
-				return $this->status($numOfChallenge);
-				break;
-			case 3:
-				$this->updateStatus();
-				return $this->status($numOfChallenge);
-				break;
-			case 4:
-				$this->updateStatus();
-				return $this->add($numOfChallenge);
-				break;
-			case 5:
-				$this->updateStatus();
-				return $this->status($numOfChallenge);
-				break;
-			case 6:
-				$this->updateStatus();
-				return $this->commit($numOfChallenge);
-				break;
-			case 7:
-				$this->updateStatus();
-				return $this->add($numOfChallenge);
-				break;
-			case 8:
-				$this->updateStatus();
-				return $this->commit($numOfChallenge);
-				break;
-			case 9:
-				$this->updateStatus();
-				return $this->log($numOfChallenge);
-				break;
-			case 10:
-				$this->updateStatus();
-				return $this->remote($numOfChallenge);
-				break;
-			case 11:
-				$this->updateStatus();
-				return $this->push($numOfChallenge);
-				break;
-			case 12:
-				$this->updateStatus();
-				return $this->pull($numOfChallenge);
-				break;
-			case 13:
-				$this->updateStatus();
-				return $this->diff($numOfChallenge);
-				break;
-			case 14:
-				$this->updateStatus();
-				return $this->add($numOfChallenge);
-				break;
-			case 15:
-				$this->updateStatus();
-				return $this->diff($numOfChallenge);
-				break;
-			case 16:
-				$this->updateStatus();
-				return $this->reset($numOfChallenge);
-				break;
-			case 17:
-				$this->updateStatus();
-				return $this->checkout($numOfChallenge);
-				break;
-			case 18:
-				$this->updateStatus();
-				return $this->branch($numOfChallenge);
-				break;
-			case 19:
-				$this->updateStatus();
-				return $this->checkout($numOfChallenge);
-				break;
-			case 20:
-				$this->updateStatus();
-				return $this->rm($numOfChallenge);
-				break;
-			case 21:
-				$this->updateStatus();
-				return $this->commit($numOfChallenge);
-				break;
-			case 22:
-				$this->updateStatus();
-				return $this->checkout($numOfChallenge);
-				break;
-			case 23:
-				$this->updateStatus();
-				return $this->merge($numOfChallenge);
-				break;
-			case 24:
-				$this->updateStatus();
-				return $this->branch($numOfChallenge);
-				break;
-			case 25:
-				$this->updateStatus();
-				return $this->push($numOfChallenge);
-				break;
-			}
+        if($isCommandValid === true) {
+            switch ($numOfChallenge) {
+            case 1: 
+                $this->updateStatus();
+                return $this->init($numOfChallenge);
+                break;
+            case 2:
+                $this->updateStatus();
+                return $this->status($numOfChallenge);
+                break;
+            case 3:
+                $this->updateStatus();
+                return $this->status($numOfChallenge);
+                break;
+            case 4:
+                $this->updateStatus();
+                return $this->add($numOfChallenge);
+                break;
+            case 5:
+                $this->updateStatus();
+                return $this->status($numOfChallenge);
+                break;
+            case 6:
+                $this->updateStatus();
+                return $this->commit($numOfChallenge);
+                break;
+            case 7:
+                $this->updateStatus();
+                return $this->add($numOfChallenge);
+                break;
+            case 8:
+                $this->updateStatus();
+                return $this->commit($numOfChallenge);
+                break;
+            case 9:
+                $this->updateStatus();
+                return $this->log($numOfChallenge);
+                break;
+            case 10:
+                $this->updateStatus();
+                return $this->remote($numOfChallenge);
+                break;
+            case 11:
+                $this->updateStatus();
+                return $this->push($numOfChallenge);
+                break;
+            case 12:
+                $this->updateStatus();
+                return $this->pull($numOfChallenge);
+                break;
+            case 13:
+                $this->updateStatus();
+                return $this->diff($numOfChallenge);
+                break;
+            case 14:
+                $this->updateStatus();
+                return $this->add($numOfChallenge);
+                break;
+            case 15:
+                $this->updateStatus();
+                return $this->diff($numOfChallenge);
+                break;
+            case 16:
+                $this->updateStatus();
+                return $this->reset($numOfChallenge);
+                break;
+            case 17:
+                $this->updateStatus();
+                return $this->checkout($numOfChallenge);
+                break;
+            case 18:
+                $this->updateStatus();
+                return $this->branch($numOfChallenge);
+                break;
+            case 19:
+                $this->updateStatus();
+                return $this->checkout($numOfChallenge);
+                break;
+            case 20:
+                $this->updateStatus();
+                return $this->rm($numOfChallenge);
+                break;
+            case 21:
+                $this->updateStatus();
+                return $this->commit($numOfChallenge);
+                break;
+            case 22:
+                $this->updateStatus();
+                return $this->checkout($numOfChallenge);
+                break;
+            case 23:
+                $this->updateStatus();
+                return $this->merge($numOfChallenge);
+                break;
+            case 24:
+                $this->updateStatus();
+                return $this->branch($numOfChallenge);
+                break;
+            case 25:
+                $this->updateStatus();
+                return $this->push($numOfChallenge);
+                break;
+            }
 
-		} else { 
-			return false;
-		}
-	}
+        } else { 
+            return false;
+        }
+    }
 
-	/**
-	 * This function explode command to 3 part depending on "space
-	 * character".
-	 * 
-	 * @param $command {string} git command like "git init".
-	 * @return {array:string}
-	 * @author Hachem Zerdia
-	 **/
-	private function explodeCommand($command) {
-		return explode(' ',$command,3);
-	}
+    /**
+     * This function explode command to 3 part depending on "space
+     * character".
+     * 
+     * @param $command {string} git command like "git init".
+     * @return {array:string}
+     * @author Hachem Zerdia
+     **/
+    private function explodeCommand($command) {
+        return explode(' ',$command,3);
+    }
 
-	/**
-	 * This function check if command is a git command "needed".
-	 * 
-	 * @param $command {string} git command like "git init".
-	 * @param $numOfChallenge {int} challenge number "status".
-	 * @return {bool}
-	 * @author Hachem Zerdia
-	 **/
+    /**
+     * This function check if command is a git command "needed".
+     * 
+     * @param $command {string} git command like "git init".
+     * @param $numOfChallenge {int} challenge number "status".
+     * @return {bool}
+     * @author Hachem Zerdia
+     **/
     private function checkCommand($command,$numOfChallenge) {
 
         $isGit = ($command[0] === 'git') ? true : false;
@@ -172,20 +172,20 @@ class CommandLine extends Git {
 
 
         if ($isGit && $isGitCommand && $isGitFlag)
-			return true;
-		else 
-			return false;
-	}
+            return true;
+        else 
+            return false;
+    }
 
-	/**
-	 * This function update challenge status.
-	 * 
-	 * @return viod
-	 * @author Hachem Zerdia
-	 **/
-	private function updateStatus() {
-		$expire = (time() + 60 * 60 * 2); // 2h.
-		setcookie('numOfChallenge',$_COOKIE['numOfChallenge'] + 1,$expire);
-	}
+    /**
+     * This function update challenge status.
+     * 
+     * @return viod
+     * @author Hachem Zerdia
+     **/
+    private function updateStatus() {
+        $expire = (time() + 60 * 60 * 2); // 2h.
+        setcookie('numOfChallenge',$_COOKIE['numOfChallenge'] + 1,$expire);
+    }
 
 } // END class CommandLine
--- core/classes/Documentation.php
+++ core/classes/Documentation.php
@@ -11,31 +11,31 @@ class Documentation {
     // array of files ~ paths (docs)
     // exmple :
     //    '1' => '../docs/initializing.html' 
-	private $Docs = NULL;
+    private $Docs = NULL;
 
-	public function __construct() {
-		$this->Docs = array(
-			'1' => BASE_PATH . '/docs/initializing.html',
-			'2' => BASE_PATH . ''
-		);
-	}
+    public function __construct() {
+        $this->Docs = array(
+            '1' => BASE_PATH . '/docs/initializing.html',
+            '2' => BASE_PATH . ''
+        );
+    }
 
-	/**
-	 * This function return challenge documentation.
-	 * 
-	 * @param $numOfChallenge {int} challenge number.
-	 * @return {string} documentation of challenge "html".
-	 * @author Hachem Zerdia
-	 **/
-	public function getDoc($numOfChallenge) {
+    /**
+     * This function return challenge documentation.
+     * 
+     * @param $numOfChallenge {int} challenge number.
+     * @return {string} documentation of challenge "html".
+     * @author Hachem Zerdia
+     **/
+    public function getDoc($numOfChallenge) {
         if (!empty($this->Docs[$numOfChallenge]))
-		    return file_get_contents($this->Docs[$numOfChallenge]);
+            return file_get_contents($this->Docs[$numOfChallenge]);
         else
             return null;
-	}
+    }
 
-	function __destruct() {
-		$this->Docs = NULL;
-	}
+    function __destruct() {
+        $this->Docs = NULL;
+    }
 
 } // END class Documentation
